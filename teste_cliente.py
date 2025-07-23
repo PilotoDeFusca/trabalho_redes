afefe2a525a35c916e60fa0d1b06808aff4cd387
PilotoDeFusca <ph4581@gmail.com>
2025-07-23 18:34:02 -0300

    Comentários

--- teste_cliente.py
+++ teste_cliente.py
@@ -4,22 +4,31 @@ import chess
 HOST = '127.0.0.1'  # IP do servidor
 PORT = 65432        # Mesma porta usada pelo servidor
 
+
+#Conecta com servidor
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    board=chess.Board()
-    while True:
 
+    #Inicializa o tabuleiro
+    board=chess.Board()
+    print(board)
+
+    #Envia o lance
+    while True:
         msg = input("Digite um lance (ou 'sair'): ")
         if msg.lower() == 'sair':
             break
-
         s.sendall(msg.encode())
+
+        #Recebe a resposta do servidor
         server_move = s.recv(1024).decode("utf-8")
 
+        #Se lance ilegal, pede novo movimento
         if server_move == "Lance ilegal":
             print("Lance ilegal")
             continue
-
+    
+        #Se lance legal, realiza movimento no tabuleiro local
         server_move_uci=chess.Move.from_uci(server_move)
         board.push(server_move_uci)
         print(f"Lance confirmado: {server_move}")
--- teste_server.py
+++ teste_server.py
@@ -1,7 +1,6 @@
 import socket
 import chess
 
-board=chess.Board()
 
 
 # Configurações do servidor
@@ -11,22 +10,37 @@ PORT = 65432        # Porta do servidor
 # Criação do socket TCP
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
-    s.listen()
-    print("Esperando jogador...")
-    conn, addr = s.accept()
-    with conn:
-        print(f'Recebendo movimentos de {addr}')
-        while True:
-            player_move = conn.recv(1024).decode('utf-8')
-            print(player_move)
-            if not player_move:
-                break
 
-            player_move_uci=chess.Move.from_uci(player_move)
-            if player_move_uci in board.legal_moves:
-                board.push(player_move_uci)
-                print(f"Lance: {player_move}")
-                conn.sendall(player_move.encode())
-            else:
-                print(f"Lance  ilegal: {player_move}")
-                conn.sendall('Lance ilegal'.encode())
+    #Escuta
+    while True:  
+        s.listen()
+        print("Esperando jogador...")
+
+        #Aceita a conexão com o jogador
+        conn, addr = s.accept()
+
+        #Conectado ao jogador
+        with conn:
+            print(f'Recebendo movimentos de {addr}')
+
+            #Inicia o tabuleiro global
+            board=chess.Board()
+
+            #Receber o lance do jogador ou sair
+            while True:
+                player_move = conn.recv(1024).decode('utf-8')
+                if not player_move:
+                    break
+
+                #Verifica o lance e responde
+                player_move_uci=chess.Move.from_uci(player_move)
+                if player_move_uci in board.legal_moves:
+                    board.push(player_move_uci)
+                    print(f"Lance: {player_move}")
+                    conn.sendall(player_move.encode())
+                else:
+                    print(f"Lance  ilegal: {player_move}")
+                    conn.sendall('Lance ilegal'.encode())
+
+        #Jogador sai, retorna a escutar
+        print('Jogador desconectado')
